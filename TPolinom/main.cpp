2bbd817626b64b93664dac7c9b85498deeebb074
Paradox644 <132946783+Paradox644@users.noreply.github.com>
2023-12-26 22:20:32 +0300

    Add files via upload

--- TPolinom/main.cpp
+++ TPolinom/main.cpp
@@ -7,16 +7,21 @@
 int main() {
 
 	TDynamicVector<TMonom> vec(2);
-	TMonom a(1, 2, 3, 4), b(2, 6, 7, 8), c(3, 2, 3, 4), d(5, 1, 2, 1);
+	TDynamicVector<TMonom> vec2(2);
+	TMonom a(1, 2, 3, 4), b(2, 6, 7, 8), c(3, 2, 3, 4), d(5,3,1,1),x(7,2,3,4), y(9,6,7,8);
 	vec[0] = a;
 	vec[1] = b;
+	vec2[0] = x;
+	vec2[1] = d;
 	TPolinom polinom(vec);
-
-	cout << "Initial value: " << polinom.ToString();
-	polinom.AddMonom(c);
-	cout << "\nFirst monom added: "<<polinom.ToString();
-	polinom.AddMonom(d);
-	cout << "\nSecond monom added: "<<polinom.ToString() << endl;
+	TPolinom polinom2(vec2);
+	cout << "Initial values: " << polinom.ToString()<< "\n" << polinom2.ToString();
+	polinom.AddPolinom(polinom,polinom2);
+	std::cout << "\nresult "<<polinom.ToString();
+	//polinom.addmonom(c);
+	//cout << "\nfirst monom added: "<<polinom.tostring();
+	//polinom.addmonom(d);
+	//cout << "\nSecond monom added: "<<polinom.ToString() << endl;
 	
 
 }
